b3b4fc9a32f805ba172518e7dd52f9c3d5cf0681
Jack Baron <jackmbaron@gmail.com>
2019-07-02 07:52:06 +0100

    canvas context

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,6 +1,15 @@
 const input = document.getElementById('input') as HTMLInputElement | null
 if (!input) throw new Error('Input box not found!')
 
+const canvas = document.getElementById('canvas') as HTMLCanvasElement | null
+if (!canvas) throw new Error('Canvas not found!')
+
+canvas.width = 512
+canvas.height = 512
+
+const ctx = canvas.getContext('2d')
+if (!ctx) throw new Error('Canvas context not found!')
+
 input.addEventListener('change', () => {
   const file = input && input.files && input.files[0]
   if (!file) return
