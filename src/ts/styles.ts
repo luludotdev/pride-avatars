ca49f6682eb73a98c5da1ce43d4e804a69c6dfe5
Jack Baron <jackmbaron@gmail.com>
2019-07-02 19:30:20 +0100

    apply styles

---
+++ src/ts/styles.ts
@@ -0,0 +1,22 @@
+import { canvas } from './canvas'
+import { save } from './dom'
+
+import { button } from '../css/button.css'
+import { canvas as cvs } from '../css/canvas.css'
+import { container, footer } from '../css/global.css'
+import { sliders } from '../css/sliders.css'
+
+canvas.classList.add(cvs)
+save.classList.add(button)
+
+const applyClass = (oldClass: string, newClass: string) => {
+  const elements = document.getElementsByClassName(oldClass)
+  for (const e of elements) {
+    e.classList.remove(oldClass)
+    e.classList.add(newClass)
+  }
+}
+
+applyClass('container', container)
+applyClass('footer', footer)
+applyClass('sliders', sliders)
