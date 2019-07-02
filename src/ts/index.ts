1613a8d8724bd348ad88e16bfddd71bfdda89459
Jack Baron <jackmbaron@gmail.com>
2019-07-02 16:59:54 +0100

    add HMR

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,22 +1,3 @@
-import { input, padding } from './input'
-import { render } from './render'
-import { state } from './state'
+import './main'
 
-input.addEventListener(
-  'change',
-  () => {
-    const file = input && input.files && input.files[0]
-    if (!file) return
-
-    const img = new Image()
-    img.src = URL.createObjectURL(file)
-    state.image = img
-  },
-  false
-)
-
-padding.addEventListener('input', () => {
-  state.padding = parseInt(padding.value, 10)
-})
-
-state.addListener('changed', () => render())
+if (module.hot) module.hot.accept()
---
+++ src/ts/main.ts
@@ -0,0 +1,22 @@
+import { input, padding } from './input'
+import { render } from './render'
+import { state } from './state'
+
+input.addEventListener(
+  'change',
+  () => {
+    const file = input && input.files && input.files[0]
+    if (!file) return
+
+    const img = new Image()
+    img.src = URL.createObjectURL(file)
+    state.image = img
+  },
+  false
+)
+
+padding.addEventListener('input', () => {
+  state.padding = parseInt(padding.value, 10)
+})
+
+state.addListener('changed', () => render())
