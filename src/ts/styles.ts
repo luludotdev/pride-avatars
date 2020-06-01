102e6db111a889ba793efda08993193500b158f5
Jack Baron <jackmbaron@gmail.com>
2020-06-01 11:11:38 +0100

    rename /css to /styles

--- src/ts/styles.ts
+++ src/ts/styles.ts
@@ -1,11 +1,11 @@
 import { canvas } from './canvas'
 import { save } from './dom'
 
-import { button } from '../css/button.styl'
-import { canvas as cvs } from '../css/canvas.styl'
-import { file } from '../css/file.styl'
-import { container, footer } from '../css/global.styl'
-import { input } from '../css/input.styl'
+import { button } from '../styles/button.styl'
+import { canvas as cvs } from '../styles/canvas.styl'
+import { file } from '../styles/file.styl'
+import { container, footer } from '../styles/global.styl'
+import { input } from '../styles/input.styl'
 
 canvas.classList.add(cvs)
 save.classList.add(button)
