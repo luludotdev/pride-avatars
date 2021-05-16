95002fd013e0d3c60573e675af18757e78d11501
Jack Baron <jackmbaron@gmail.com>
2021-05-16 18:07:00 +0100

    nuke everything in prep for rewrite

--- src/ts/firefox.ts
+++
@@ -1,6 +0,0 @@
-import { firefox__hidden as hidden } from '../styles/firefox.styl'
-
-if (navigator.userAgent.toLowerCase().includes('firefox')) {
-  const element = document.querySelector(`.${hidden}`)
-  if (element !== null) element.classList.remove(hidden)
-}
