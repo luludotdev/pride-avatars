4faf1864d4f8289feb17263ad4cfcf456c76d270
Jack Baron <jackmbaron@gmail.com>
2021-05-16 22:13:40 +0100

    handle drag and drop on canvas

--- components/app/Canvas.tsx
+++ components/app/Canvas.tsx
@@ -1,7 +1,8 @@
+import { useCallback } from 'react'
 import { useAnimationFrame } from '~lib/hooks/useAnimationFrame'
 import { useStore } from '~lib/hooks/useStore'
 import { drawFrame } from '~lib/render'
-import type { FC, RefObject } from 'react'
+import type { DragEventHandler, FC, RefObject } from 'react'
 
 interface Props {
   children?: never
@@ -22,12 +23,38 @@ export const Canvas: FC<Props> = ({ canvasRef: ref }) => {
     dispatch({ type: 'markClean' })
   }, [])
 
+  const handleDragOver = useCallback<DragEventHandler<HTMLCanvasElement>>(
+    ev => {
+      ev.preventDefault()
+    },
+    []
+  )
+
+  const handleDrop = useCallback<DragEventHandler<HTMLCanvasElement>>(
+    ev => {
+      ev.preventDefault()
+      if (!ev.dataTransfer) return
+
+      const file = ev.dataTransfer.items
+        ? ev.dataTransfer.items[0].getAsFile()
+        : ev.dataTransfer.files[0]
+
+      if (!file) return
+      if (!file.type.startsWith('image/')) return
+
+      dispatch({ type: 'setImage', value: URL.createObjectURL(file) })
+    },
+    [dispatch]
+  )
+
   return (
     <canvas
       ref={ref}
       className='w-full h-auto rounded'
       width='512'
       height='512'
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
     />
   )
 }
