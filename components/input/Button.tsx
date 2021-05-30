a053683fbcc111b06de547e74298fafe755ca947
Jack Baron <jackmbaron@gmail.com>
2021-05-30 21:08:51 +0100

    style disabled buttons

--- components/input/Button.tsx
+++ components/input/Button.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import { useCallback } from 'react'
 import type { FC } from 'react'
 
@@ -15,7 +16,27 @@ export const Button: FC<Props> = ({ disabled, onClick, children }) => {
     <button
       type='button'
       disabled={disabled}
-      className='w-full py-2 font-semibold text-lg rounded border border-gray-400 dark:border-gray-500 transition-all focus:outline-none hover:border-gray-100 dark:hover:border-gray-300 hover:shadow-md dark:hover:shadow-none'
+      className={clsx(
+        'w-full',
+        'py-2',
+        'font-semibold',
+        'text-lg',
+        'rounded',
+        'border',
+        'border-gray-400',
+        'dark:border-gray-500',
+        'transition-all',
+        'focus:outline-none',
+        'hover:border-gray-100',
+        'dark:hover:border-gray-300',
+        'hover:shadow-md',
+        'dark:hover:shadow-none',
+        'disabled:opacity-40',
+        'disabled:shadow-none',
+        'disabled:cursor-not-allowed',
+        'disabled:hover:border-gray-400',
+        'dark:disabled:hover:border-gray-500'
+      )}
       onClick={handleClick}
     >
       {children}
