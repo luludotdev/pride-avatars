2d39b8e00ef343b610eb04f47782342db7d5ce92
Jack Baron <jackmbaron@gmail.com>
2021-05-30 20:41:05 +0100

    prep for gif encoding

--- components/app/SaveImage.tsx
+++ components/app/SaveImage.tsx
@@ -1,5 +1,6 @@
 import { useCallback } from 'react'
 import { Button } from '~components/input/Button'
+import { useStore } from '~lib/hooks/useStore'
 import type { FC, RefObject } from 'react'
 
 interface Props {
@@ -8,18 +9,23 @@ interface Props {
 }
 
 export const SaveImage: FC<Props> = ({ canvasRef }) => {
+  const { state } = useStore()
   const onSaveClicked = useCallback(() => {
     if (!canvasRef.current) return
     const canvas = canvasRef.current
 
-    const a = document.createElement('a')
-    const b64 = canvas.toDataURL('image/png;base64')
+    if (state.frames) {
+      // TODO: Implement
+    } else {
+      const a = document.createElement('a')
+      const b64 = canvas.toDataURL('image/png;base64')
 
-    a.href = b64
-    a.download = 'avatar.png'
-    a.click()
-    a.remove()
-  }, [canvasRef])
+      a.href = b64
+      a.download = 'avatar.png'
+      a.click()
+      a.remove()
+    }
+  }, [canvasRef, state])
 
   return <Button onClick={onSaveClicked}>💾 Download</Button>
 }
