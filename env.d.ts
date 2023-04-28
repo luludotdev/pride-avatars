93b93f944539b97501dbeb2768a8f63dcdeed06e
Jack Baron <jackmbaron@gmail.com>
2023-04-28 08:07:04 +0100

    cleanup manual env

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,14 @@
-declare module '~/assets/flags/*.svg' {
-  const content: import('next/image').StaticImageData
+import type * as CSS from 'csstype'
+import type { StaticImageData } from 'next/image'
 
+declare module 'csstype' {
+  interface Properties {
+    // Allow any CSS Custom Properties
+    [index: `--${string}`]: string
+  }
+}
+
+declare module '~/assets/flags/*.svg' {
+  const content: StaticImageData
   export default content
 }
