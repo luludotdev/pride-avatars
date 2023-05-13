c8c93b6390fc7b24928380db7e3f6660528aa088
Jack Baron <jackmbaron@gmail.com>
2023-05-13 06:15:14 +0100

    only create composite canvases on client

--- lib/hooks/useComposite.ts
+++ lib/hooks/useComposite.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react'
+import { useCallback, useEffect, useMemo } from 'react'
 import type { ConditionalPick, SetNonNullable } from 'type-fest'
 import { useDebug } from '~/lib/hooks/useDebug'
 
@@ -11,52 +11,66 @@ export type CompositeCtx = ConditionalPick<
 export const useComposite = () => {
   const debug = useDebug()
 
-  const imageCanvas = useMemo(() => document.createElement('canvas'), [])
-  const maskCanvas = useMemo(() => document.createElement('canvas'), [])
-  const compCanvas = useMemo(() => document.createElement('canvas'), [])
+  const isServer = useMemo(() => typeof window === 'undefined', [])
+  const createCanvas = useCallback(() => {
+    if (isServer) return undefined
+    return document.createElement('canvas')
+  }, [isServer])
+
+  const imageCanvas = useMemo(() => createCanvas(), [createCanvas])
+  const maskCanvas = useMemo(() => createCanvas(), [createCanvas])
+  const compCanvas = useMemo(() => createCanvas(), [createCanvas])
 
   useEffect(() => {
     if (!debug) return
-
-    document.body.appendChild(imageCanvas)
-    document.body.appendChild(maskCanvas)
-    document.body.appendChild(compCanvas)
 
     const spacing = '1rem'
     const size = '25rem'
     const bg = 'red'
 
-    imageCanvas.style.position = 'absolute'
-    imageCanvas.style.backgroundColor = bg
-    imageCanvas.style.top = spacing
-    imageCanvas.style.left = spacing
-    imageCanvas.style.width = size
-    imageCanvas.style.height = size
+    if (imageCanvas) {
+      document.body.appendChild(imageCanvas)
 
-    maskCanvas.style.position = 'absolute'
-    maskCanvas.style.backgroundColor = bg
-    maskCanvas.style.top = `calc(${size} + (${spacing} * 2))`
-    maskCanvas.style.left = spacing
-    maskCanvas.style.width = size
-    maskCanvas.style.height = size
+      imageCanvas.style.position = 'absolute'
+      imageCanvas.style.backgroundColor = bg
+      imageCanvas.style.top = spacing
+      imageCanvas.style.left = spacing
+      imageCanvas.style.width = size
+      imageCanvas.style.height = size
+    }
 
-    compCanvas.style.position = 'absolute'
-    compCanvas.style.backgroundColor = bg
-    compCanvas.style.top = `calc((${size} * 2) + (${spacing} * 3))`
-    compCanvas.style.left = spacing
-    compCanvas.style.width = size
-    compCanvas.style.height = size
+    if (maskCanvas) {
+      document.body.appendChild(maskCanvas)
+
+      maskCanvas.style.position = 'absolute'
+      maskCanvas.style.backgroundColor = bg
+      maskCanvas.style.top = `calc(${size} + (${spacing} * 2))`
+      maskCanvas.style.left = spacing
+      maskCanvas.style.width = size
+      maskCanvas.style.height = size
+    }
+
+    if (compCanvas) {
+      document.body.appendChild(compCanvas)
+
+      compCanvas.style.position = 'absolute'
+      compCanvas.style.backgroundColor = bg
+      compCanvas.style.top = `calc((${size} * 2) + (${spacing} * 3))`
+      compCanvas.style.left = spacing
+      compCanvas.style.width = size
+      compCanvas.style.height = size
+    }
 
     return () => {
-      imageCanvas.remove()
-      maskCanvas.remove()
-      compCanvas.remove()
+      imageCanvas?.remove()
+      maskCanvas?.remove()
+      compCanvas?.remove()
     }
   }, [debug, imageCanvas, maskCanvas, compCanvas])
 
-  const ctxImage = useMemo(() => imageCanvas.getContext('2d'), [imageCanvas])
-  const ctxMask = useMemo(() => maskCanvas.getContext('2d'), [maskCanvas])
-  const ctxComp = useMemo(() => compCanvas.getContext('2d'), [compCanvas])
+  const ctxImage = useMemo(() => imageCanvas?.getContext('2d'), [imageCanvas])
+  const ctxMask = useMemo(() => maskCanvas?.getContext('2d'), [maskCanvas])
+  const ctxComp = useMemo(() => compCanvas?.getContext('2d'), [compCanvas])
 
   return { imageCanvas, ctxImage, maskCanvas, ctxMask, compCanvas, ctxComp }
 }
