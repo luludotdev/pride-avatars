60c89d41257af390aa19ee20712acff99f731b87
Jack Baron <jackmbaron@gmail.com>
2024-01-29 08:55:06 +0000

    cleanup now unused components

--- components/app/Warning.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC, PropsWithChildren } from 'react'
-
-export const Warning: FC<PropsWithChildren<unknown>> = ({ children }) => (
-  <div className='flex w-full flex-col gap-y-1 rounded border border-yellow-400 bg-yellow-200 px-4 py-3 text-center dark:text-black'>
-    <p className='text-lg font-semibold'>⚠️ Warning!</p>
-    {children}
-  </div>
-)
