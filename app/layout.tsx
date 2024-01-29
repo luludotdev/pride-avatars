05f938e5f489d18b4e4b6278b37719ca5d132857
Jack Baron <jackmbaron@gmail.com>
2024-01-29 06:40:11 +0000

    update root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,38 @@
 import './tailwind.css'
 
-import { clsx } from 'clsx'
 import { Fira_Code as FiraCode, Inter } from 'next/font/google'
-import type { FC, PropsWithChildren } from 'react'
+import type { ReactNode } from 'react'
 import { PreloadFlags } from '~/components/PreloadFlags'
+import { cn } from '~/lib/utils'
 
-const inter = Inter({
-  variable: '--font-inter',
+const fontSans = Inter({
+  variable: '--font-sans',
   subsets: ['latin'],
   display: 'swap',
   weight: ['500', '600', '700'],
 })
 
-const firaCode = FiraCode({
-  variable: '--font-fira-code',
+const fontMono = FiraCode({
+  variable: '--font-mono',
   subsets: ['latin'],
   display: 'swap',
   weight: ['500'],
 })
 
-const RootLayout: FC<PropsWithChildren> = ({ children }) => (
-  <html className={clsx(inter.variable, firaCode.variable)} lang='en'>
+const RootLayout = ({ children }: { readonly children: ReactNode }) => (
+  <html lang='en'>
     <head>
       <PreloadFlags />
     </head>
 
-    <body className='bg-light dark:bg-dark dark:text-light'>
-      <div className='flex h-screen w-full flex-col items-center px-4'>
-        {children}
-      </div>
+    <body
+      className={cn(
+        'bg-background min-h-screen font-sans antialiased',
+        fontSans.variable,
+        fontMono.variable,
+      )}
+    >
+      {children}
     </body>
   </html>
 )
