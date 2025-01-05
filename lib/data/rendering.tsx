cd5f8bb99754ad2e1554a86623d0454ab3468828
Jack Baron <jackmbaron@gmail.com>
2025-01-05 10:33:35 +0000

    update context

--- lib/data/rendering.tsx
+++ lib/data/rendering.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { createContext, useContext, useMemo, useRef } from "react";
+import { createContext, use, useMemo, useRef } from "react";
 import type { ReactNode, RefObject } from "react";
 import { useLayers as useMakeLayers } from "~/lib/hooks/useLayers";
 import type { MaybeLayers } from "~/lib/layers";
 
 const Context = createContext<{
-  readonly canvas: RefObject<HTMLCanvasElement>;
+  readonly canvas: RefObject<HTMLCanvasElement | null>;
   readonly layers: MaybeLayers;
 }>({
   canvas: { current: null },
@@ -28,15 +28,15 @@ export const CanvasProvider = ({
   const layers = useMakeLayers();
 
   const value = useMemo(() => ({ canvas: ref, layers }), [ref, layers]);
-  return <Context.Provider value={value}>{children}</Context.Provider>;
+  return <Context value={value}>{children}</Context>;
 };
 
 export const useCanvas = () => {
-  const { canvas } = useContext(Context);
+  const { canvas } = use(Context);
   return canvas;
 };
 
 export const useLayers = () => {
-  const { layers } = useContext(Context);
+  const { layers } = use(Context);
   return layers;
 };
