51efb51b068710b9c08c513c2635499cc8161c1d
Jack Baron <jackmbaron@gmail.com>
2025-01-05 10:23:37 +0000

    optical sizing

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,10 @@ const Root = () => (
 
     <Container>
       <Content>
-        <h1 className="my-2 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        <h1
+          className="my-2 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"
+          style={{ fontVariationSettings: '"opsz" 14' }}
+        >
           Pride Avatars 🏳️‍🌈
         </h1>
 
